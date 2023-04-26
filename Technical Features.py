154456eba6cd761bf2ed3e21a8a4d88dceb33859
metraats <104796775+metraats@users.noreply.github.com>
2023-04-26 17:41:39 +0300

    Update Technical Features.py

--- Technical Features.py
+++ Technical Features.py
@@ -225,17 +225,6 @@ def PastReturns(price):
 
 
 
-#Trade Flow Imbalance
-def TFI(asks_amounts, bids_amounts):
-    
-    Buy_volume = bids_amounts.sum(axis = 1)
-    Sell_volume = asks_amounts.sum(axis = 1)
-    TFI = Buy_volume - Sell_volume
-    
-    return TFI
-
-
-
 #Mean Divirgence
 def MeanDivirgence(prices_1, prices_2, window = 10, fillnans = True):
     
