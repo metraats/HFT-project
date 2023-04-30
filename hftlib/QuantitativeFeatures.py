a51b37d62290e885a344f2390aac97d86212465d
metraats <104796775+metraats@users.noreply.github.com>
2023-04-30 22:10:51 +0300

    Add files via upload

---
+++ hftlib/QuantitativeFeatures.py
@@ -0,0 +1,261 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+
+#Order Book Imbalance
+def OBI(asks_amounts, bids_amounts):
+    Va = asks_amounts.mean(axis = 1)
+    Vb = bids_amounts.mean(axis = 1)
+
+    OBI = (Vb-Va)/(Va+Vb)
+    return OBI
+
+
+
+#Autocorrelation (can be used not only for price time series)
+def Autocorrelation(price, window=100, lag=10, fillnans = True, delete_outliers=True):
+    shifted = price.shift(lag)
+    correlation = price.rolling(window).corr(shifted)
+
+    if delete_outliers:
+        correlation[abs(correlation) > 1] = np.nan
+
+    if fillnans:
+        correlation.fillna(method = 'ffill', inplace = True)
+
+    return correlation
+
+
+
+#Partial Correlation (can be used not only for price time series)
+def PartialCorrelation(price, window=100, lag=10, fillnans = True):
+    prices_copy = price.copy()
+    answ = pd.DataFrame(index = price.index)
+
+    for i in range(1, lag + 1):
+        shifted = prices_copy.shift(i)
+        correlation = prices_copy.rolling(window).corr(shifted)
+
+        if fillnans:
+            correlation.fillna(method = 'ffill', inplace = True)
+
+        answ[f'PACF for lag {i}'] = correlation
+        prices_copy -= correlation * shifted
+
+    return answ
+
+
+
+#Cointegration
+def Cointegration(asks_amounts, bids_amounts, window=10, fillnans=True, method = 'total volume', depth = 1):
+    if method == 'by level':
+
+        b1 = pd.DataFrame(index = ASK.index)
+        b0 = pd.DataFrame(index = ASK.index)
+
+        for i in range(depth):
+            y = asks_amounts
+            X = bids_amounts
+
+            X_mean = X.rolling(window).mean()
+            y_mean = y.rolling(window).mean()
+
+            XminusX_mean = X - X_mean
+            YminusY_mean = y - y_mean
+
+            b_1 = (XminusX_mean * YminusY_mean).rolling(window).sum() / (XminusX_mean * XminusX_mean).rolling(window).sum()#ddd
+            b_1[b_1 == np.inf] = np.nan
+            b_1[b_1 == -np.inf] = np.nan
+            b_0 = y_mean - b_1 * X_mean
+
+            b1[f'LVL {i}'] = b_1
+            b0[f'LVL {i}'] = b_0
+
+    elif method == 'total volume':
+        y = asks_amounts.sum(axis = 1)
+        X = bids_amounts.sum(axis = 1)
+
+        X_mean = X.rolling(window).mean()
+        y_mean = y.rolling(window).mean()
+
+        XminusX_mean = X - X_mean
+        YminusY_mean = y - y_mean
+
+        b1 = (XminusX_mean * YminusY_mean).rolling(window).sum() / (XminusX_mean * XminusX_mean).rolling(window).sum()#ddd
+        b1[b1 == np.inf] = np.nan
+        b1[b1 == -np.inf] = np.nan
+        b0 = y_mean - b1 * X_mean
+
+    return b0, b1
+
+
+
+#Adaptive Logistic Regression
+def AdaptiveLogreg(asks_amounts=None, bids_amounts=None, ask_lowest_price=None, bid_highest_price=None,
+                   window=10, lag=1, method='binary', side = None, depth = 6):
+    #Сам логрег
+    def logregfunc(X, xtest, y, method='binary'):
+        lr = LogisticRegression(solver = 'newton-cg')
+        lr.fit(X,y)
+
+        if method=='triple':
+            if set(y.unique()) == set([-1, 1]):
+                ret = np.array([lr.predict_proba(xtest)[0][0], 0, lr.predict_proba(xtest)[0][1]])
+            elif set(y.unique()) == set([0, 1]):
+                ret = np.array([0, lr.predict_proba(xtest)[0][0], lr.predict_proba(xtest)[0][1]])
+            elif set(y.unique()) == set([-1, 0]):
+                ret = np.array([lr.predict_proba(xtest)[0][0], lr.predict_proba(xtest)[0][1], 0])
+            else:
+                ret = np.array([lr.predict_proba(xtest)[0][0], lr.predict_proba(xtest)[0][1], lr.predict_proba(xtest)[0][2]])
+
+        if method=='binary':
+            ret = np.array([lr.predict_proba(xtest)[0][0], lr.predict_proba(xtest)[0][1]])
+
+        return ret
+
+    # Для аска
+    if side == 'ask':
+        ASK = asks_amounts
+        ASK.fillna(0, inplace=True)
+
+        # Изменение/не изменение
+        if method == 'binary':
+            tgts = (ask_lowest_price != ask_lowest_price.shift(lag)) * 1
+            tgts.fillna(0, inplace=True)
+            answers = []
+
+            for i in range(window, ASK.shape[0]-1):
+                X = ASK[i-window:i]
+                y = tgts[i-window:i]
+                xtest = ASK.iloc[i:i+1]
+
+                if sum(abs(y)) == 0:
+                    preds = np.array([1,0])
+                elif sum(y) == window:
+                    preds = np.array([0,1])
+                else:
+                    preds = logregfunc(X, xtest, y, method='binary')
+                answers.append(preds)
+            answ = pd.DataFrame(answers, columns=['Ask price=','Ask price+-'])
+
+        # 0/рост/падние
+        if method == 'triple':
+            tgts = ((ask_lowest_price > ask_lowest_price.shift(lag)) * 1) + ((ask_lowest_price < ask_lowest_price.shift(lag)) * -1)
+            tgts.fillna(0, inplace=True)
+            answers = []
+
+            for i in range(window, ASK.shape[0]-1):
+                X = ASK[i-window:i]
+                y = tgts[i-window:i]
+                xtest = ASK.iloc[i:i+1]
+
+                if sum(abs(y)) == 0:
+                    preds = np.array([0,1,0])
+                elif sum(y) == window:
+                    preds = np.array([0,0,1])
+                elif sum(y) == -window:
+                    preds = np.array([1,0,0])
+                else:
+                    preds = logregfunc(X, xtest, y, method='triple')
+                answers.append(preds)
+            answ = pd.DataFrame(answers, columns=['Ask price-', 'Ask price=', 'Ask price+'])
+
+    # Для бида
+    if side == 'bid':
+        BIDS = bids_amounts
+        BIDS.fillna(0, inplace=True)
+
+        #Изменение/не изменение
+        if method == 'binary':
+            tgts = (bid_highest_price != bid_highest_price.shift(lag)) * 1
+            tgts.fillna(0, inplace=True)
+            answers = []
+
+            for i in range(window, BIDS.shape[0]-1):
+                X = BIDS[i-window:i]
+                y = tgts[i-window:i]
+                xtest = BIDS.iloc[i:i+1]
+
+                if sum(abs(y)) == 0:
+                    preds = np.array([1,0])
+                elif sum(y) == window:
+                    preds = np.array([0,1])
+                else:
+                    preds = logregfunc(X, xtest, y, method='binary')
+                answers.append(preds)
+            answ = pd.DataFrame(answers, columns=['Bid price=','Bid price+-'])
+
+        # 0/рост/падние
+        if method == 'triple':
+            tgts = ((bid_highest_price > bid_highest_price.shift(lag)) * 1) + ((bid_highest_price < bid_highest_price.shift(lag)) * -1)
+            tgts.fillna(0, inplace=True)
+            answers = []
+
+            for i in range(window, BIDS.shape[0]-1):
+                X = BIDS[i-window:i]
+                y = tgts[i-window:i]
+                xtest = BIDS.iloc[i:i+1]
+
+                if sum(abs(y)) == 0:
+                    preds = np.array([0,1,0])
+                elif sum(y) == window:
+                    preds = np.array([0,0,1])
+                elif sum(y) == -window:
+                    preds = np.array([1,0,0])
+                else:
+                    preds = logregfunc(X, xtest, y, method='triple')
+                answers.append(preds)
+            answ = pd.DataFrame(answers, columns=['Bid price-','Bid price=','Bid price+'])
+    return answ
+
+
+
+#Order Book Internal Imbalance median (For ask and bid sides)
+def Internal_imbalance_median_ask(asks_amounts, asks_prices):
+    median_amounts = asks_amounts.median(axis = 1)
+    residuals = median_amounts
+    proportions = pd.DataFrame(index = asks_amounts.index, columns = asks_amounts.columns)
+
+    for col in asks_amounts.columns:
+        proportions[col] = pd.DataFrame([residuals, asks_amounts[col]]).min()
+        residuals = residuals - proportions[col]
+
+    Pn = 0
+    for col_amount, col_price in list(zip(proportions.columns, asks_prices.columns)):
+        Pn += proportions[col_amount] * asks_prices[col_price]
+
+    lowest_ask_price = asks_prices[asks_prices.columns[0]]
+    IMB_a = (Pn/lowest_ask_price - 1) * 10000
+
+    return IMB_a
+
+
+
+def Internal_imbalance_median_bid(bids_amounts, bids_prices):
+    median_amounts = bids_amounts.median(axis = 1)
+    residuals = median_amounts
+    proportions = pd.DataFrame(index = bids_amounts.index, columns = bids_amounts.columns)
+
+    for col in bids_amounts.columns:
+        proportions[col] = pd.DataFrame([residuals, bids_amounts[col]]).min()
+        residuals = residuals - proportions[col]
+
+    Pn = 0
+    for col_amount, col_price in list(zip(proportions.columns, bids_prices.columns)):
+        Pn += proportions[col_amount] * bids_prices[col_price]
+
+    highest_bid_price = bids_prices[bids_prices.columns[0]]
+    IMB_b = (highest_bid_price/Pn - 1) * 10000
+
+    return IMB_b
+
+
+
+#Trade Flow Imbalance
+def TFI(asks_amounts, bids_amounts):
+    Buy_volume = bids_amounts.sum(axis = 1)
+    Sell_volume = asks_amounts.sum(axis = 1)
+    TFI = Buy_volume - Sell_volume
+
+    return TFI
---
+++ hftlib/TechnicalFeatures.py
@@ -0,0 +1,428 @@
+import numpy as np
+import pandas as pd
+
+
+#AccumulationDistributionLine
+def ADL(price, amount, block=10, fillnans = True):
+    C = price.copy()
+    L = price.rolling(block).min()
+    H = price.rolling(block).max()
+
+    MoneyFlowMultiplier = (2*C-L-H)/(H-L)
+    BlockPeriodVolume = price
+
+    MoneyFlowVolumet = MoneyFlowMultiplier * amount.rolling(block).mean()
+    ADL = MoneyFlowVolumet.cumsum()
+
+    if fillnans:
+        ADL.fillna(method = 'ffill', inplace = True)
+
+    return ADL
+
+
+
+#Average directional index
+def ADX(price, block = 10, window=14, fillnans = True):
+    L = price.rolling(block).min()
+    H = price.rolling(block).max()
+    CL = price.copy()
+
+    plusM = H - H.shift(1)
+    minusM = L.shift(1) - L
+
+    plusMindicator = (plusM >= minusM) & (plusM >= 0) * 1
+    minusMindicator = (plusM <= minusM) & (minusM >= 0) * 1
+
+    plusDM = plusM * plusMindicator
+    minusDM = minusM * minusMindicator
+
+    TR = pd.concat([H, CL.shift(1)], axis = 1).max(axis = 1) - pd.concat([L, CL.shift(1)], axis = 1).min(axis = 1)
+
+    plusDI = (plusDM/TR).rolling(block*window-1).mean().shift(1) * (window*block-1)/window + (plusDM/TR)/window
+    minusDI = (minusDM/TR).rolling(block*window-1).mean().shift(1) * (block*window-1)/window + (minusDM/TR)/window
+
+    ADX = 100 * (abs(plusDI - minusDI)/(plusDI + minusDI)).rolling(block*window-1).mean().shift(1)*(block*window-1)/window + 100*(abs(plusDI - minusDI)/(plusDI + minusDI))/window
+
+    if fillnans:
+        ADX.fillna(method = 'ffill', inplace = True)
+
+    return ADX
+
+
+
+#Chande Momentum oscillator
+def CMO(price, block=10, window=19, fillnans = True):
+    updays = (price / (price.shift(1)) > 1) * 1
+    downdays = (price / (price.shift(1)) < 1) * 1
+    zerodays = (price / (price.shift(1)) == 1) * 1
+
+    CL_up = (price - (price.shift(1))) * updays
+    CL_down = -(price - (price.shift(1))) * downdays
+
+    Su = CL_up.rolling(window*block).sum()
+    Sd = CL_down.rolling(window*block).sum()
+
+    CMO = 100 * (Su - Sd)/(Su + Sd)
+
+    if fillnans:
+        CMO.fillna(method = 'ffill', inplace = True)
+
+    return CMO
+
+
+
+#Momentum
+def Momentum(price):
+    MOM = price - price.shift(1)
+
+    return MOM
+
+
+
+#Rate of Change
+def ROC(price, block=10, window=12):
+    ROC = (price - price.shift(block*window)) / price.shift(block*window) * 100
+
+    return ROC
+
+
+
+#Relative Strength Index
+def RSI(price, block=10, window=14, fillnans = True):
+    CL = price - price.shift(1)
+
+    updays = ((CL - CL.shift(1)) > 0) * 1
+    downdays = ((CL - CL.shift(1)) < 0) * 1
+    zerodays = ((CL - CL.shift(1)) == 0) * 1
+
+    CL_up = CL * updays
+    CL_down = -CL * downdays
+
+    AG = CL_up.rolling(block*window).sum()
+    AL = CL_down.rolling(block*window).sum()
+
+    Relative_strength = AG/AL
+
+    RSI = 100 - 100/(1 + Relative_strength)
+
+    if fillnans:
+        RSI.fillna(method = 'ffill', inplace = True)
+
+    return RSI
+
+
+
+#Stochastic Relative Strength Index
+def StochasticRSI(price, block=10, window=14, rsi_block = 10, fillnans = True):
+    rsi = RSI(price, block=10, window=14)
+
+    RSI_max = rsi.rolling(block*window*rsi_block).max()
+    RSI_min = rsi.rolling(block*window*rsi_block).min()
+
+    StochRSI = (rsi - RSI_min)/(RSI_max - RSI_min)
+
+    if fillnans:
+        StochRSI.fillna(method = 'ffill', inplace = True)
+
+    return StochRSI
+
+
+
+#Linear regression line
+def LinearRegressionLine(price, window_learning=10, window_forecasting=1, fillnans = True):
+    X_train = price.copy()
+    y_train = price.shift(-window_forecasting)
+
+    X_mean = X_train.rolling(window_learning).mean()
+    y_mean = y_train.rolling(window_learning).mean()
+
+    XminusX_mean = X_train - X_mean
+    YminusY_mean = y_train - y_mean
+
+    b1 = (XminusX_mean * YminusY_mean).rolling(window_learning).sum() / (XminusX_mean * XminusX_mean).rolling(window_learning).sum()#ddd
+    b1[b1 == np.inf] = np.nan
+    b1[b1 == -np.inf] = np.nan
+    b0 = y_mean - b1 * X_mean
+
+    if fillnans:
+        b1.fillna(method = 'ffill', inplace = True)
+        b0.fillna(method = 'ffill', inplace = True)
+
+    return b0, b1
+
+
+
+#Realized Volatility
+def RealizedVolatility(price, block=10, window=10, fillnans = True):
+    v = (price - price.shift(1)) * (price - price.shift(1))
+    Realized_Volatility = v.rolling(block*window).mean()
+
+    if fillnans:
+        Realized_Volatility.fillna(method = 'ffill', inplace = True)
+
+    return Realized_Volatility
+
+
+
+#Realized Kernel
+def RealizedKernel(price, horizon=7, window=10, kernel = 'Bartlett'):
+    def k(x, kernel):
+        if kernel == 'Bartlett':
+            return 1-x
+
+    returns = price - price.shift(1)
+
+    shifts_covs = pd.DataFrame(index=price.index)
+    for i in range(1,horizon+1):
+        shifts_covs[f'h={i}'] = returns.rolling(window).cov(returns.shift(i)) * window * k(x=(i-1)/horizon, kernel=kernel)
+    for i in range(-horizon,0):
+        shifts_covs[f'h={i}'] = returns.rolling(window).cov(returns.shift(i)) * window * k(x=(-i-1)/horizon, kernel=kernel)
+
+    variance = price.rolling(window).var()
+    K = variance + shifts_covs.sum(axis=1)
+
+    return K
+
+
+
+#Realized Bipower Variation
+def RealizedBipowerVariation(price, block=10, window=10, fillnans = True):
+    delta = (price-price.shift(1)) * (price-price.shift(1)).shift(1)
+    t = delta.rolling(block*window).mean()
+
+    RealizedBipowerVolatility = t * np.pi * 0.5
+
+    if fillnans:
+        RealizedBipowerVolatility.fillna(method = 'ffill', inplace = True)
+
+    return RealizedBipowerVolatility
+
+
+
+#Jump Variation
+def JumpVariation(price, block=10, window=10, fillnans = True):
+    JumpVariation = RealizedVolatility(price, block=block, window=window, fillnans = fillnans) - RealizedBipowerVariation(price, block=block, window=window, fillnans = fillnans)
+
+    return JumpVariation
+
+
+
+#Past Returns
+def PastReturns(price):
+    PRET = price.pct_change() * 10000
+    PRET.mean()
+
+    return PRET
+
+
+
+#Mean Divirgence
+def MeanDivirgence(prices_1, prices_2, block=10, window = 10, fillnans = True):
+    d = prices_1/prices_2 - 1
+    d[d == np.inf] = np.nan
+    d[d == -np.inf] = np.nan
+
+    DIV = d - d.rolling(window*block).mean()
+
+    if fillnans:
+        DIV.fillna(method = 'ffill', inplace = True)
+
+    return DIV
+
+
+
+#Awesome Oscillator
+def AwesomeOscillator(price, block = 10, window_big = 34, window_small = 5):
+    H = price.rolling(block).max()
+    L = price.rolling(block).min()
+
+    AO = (H+L).rolling(window_small*block).mean()/2 - (H+L).rolling(window_big*block).mean()/2
+
+    return AO
+
+
+
+#Accelerator Oscillator
+def AcceleratorOscillator(price, AO_window = 5, block = 10, window_big = 34, window_small = 5):
+    AO = AwesomeOscillator(price=price, block = block, window_big = window_big, window_small = window_small)
+
+    return AO - AO.rolling(AO_window).mean()
+
+
+
+#Average directional movement index rating
+def ADXR(price, block=10, window = 14, fillnans = True):
+    adx = ADX(price=price, block=block, window=window, fillnans = fillnans)
+
+    return (adx + adx.shift(1))/2
+
+
+
+#Williams Alligator Indicator
+def AlligatorIndicator(price, block=10, w_jaw=13, w_teeth=8, w_lips=5):
+    H = price.rolling(block).max()
+    L = price.rolling(block).min()
+
+    jaw = (H+L).rolling(block*w_jaw).mean()/2
+    teeth = (H+L).rolling(block*w_teeth).mean()/2
+    lips = (H+L).rolling(block*w_lips).mean()/2
+
+    return jaw, teeth, lips
+
+
+
+#Absolute price oscillator
+def APO(price, block=10, small_window=5, big_window=13):
+    H = price.rolling(block).max()
+    L = price.rolling(block).min()
+
+    M = (H+L)/2
+    APO = M.ewm(small_window*block).mean() - M.ewm(big_window*block).mean()
+
+    return APO
+
+
+
+#Average True Range
+def ATR(price, block=10, window=14, Normalized = False):
+    L = price.rolling(block*window).min()
+    H = price.rolling(block*window).max()
+    CL = price.copy()
+
+    TR = pd.concat([H-L, abs(H-CL.shift(1)), abs(L-CL.shift(1))], axis = 1).max(axis = 1)
+    ATR = TR.ewm(alpha = 1/(block*window), adjust=False).mean()
+
+    if Normalized:
+        return ATR/CL*100
+
+    return ATR
+
+
+
+#Bollinger Bands
+def Bollinger(price, block=10, window=20, k=2):
+
+    BB_std = price.rolling(block*window).std()
+    BB_middle = price.rolling(block*window).mean()
+    BB_lower = BB_middle - k*BB_std
+    BB_upper = BB_middle + k*BB_std
+
+    return BB_middle, BB_upper, BB_lower
+
+
+
+#Ichimoku clouds
+def IchimokuClouds(price, block=10, window_convline=9, window_baseline=26, window_leadingspanb=52):
+
+    ConversionLine = (price.rolling(block*window_convline).max() + price.rolling(block*window_convline).min())/2
+    Baseline = (price.rolling(block*window_baseline).max() + price.rolling(block*window_baseline).min())/2
+    LeadingSpanA = (ConversionLine+Baseline)/2
+    LeadingSpanB = (price.rolling(block*window_leadingspanb).max() + price.rolling(block*window_leadingspanb).min())/2
+    LaggingSpan = price.shift(block*window_baseline)
+
+    return ConversionLine, Baseline, LeadingSpanA, LeadingSpanB, LaggingSpan
+
+
+
+#Chaikin Oscillator
+def ChaikinOscillator(price, volumes, block=10, ema1 = 3, ema2 = 10):
+
+    L = price.rolling(block).min()
+    H = price.rolling(block).max()
+    V = volumes.rolling(block).sum()
+
+    N = (2*price - L - H)/(H - L)
+    M = N * V
+    ADL = M + M.shift(1)
+    CO = ADL.ewm(ema1).mean() - ADL.ewm(ema2).mean()
+
+    return CO
+
+
+
+#Chandelier Exit
+def ChandelierExit(price, block=10, window=22, k=3):
+
+    L = price.rolling(block*window).min()
+    H = price.rolling(block*window).max()
+
+    ChandelierLong = H - k * ATR(price, block=block, window=window)
+    ChandelierShort = L + k * ATR(price, block=block, window=window)
+
+    return ChandelierLong, ChandelierShort
+
+
+
+#Center of gravity
+def COG(price, block = 10, n = 1):
+    W = price.copy()
+
+    for i in range(1, block*n):
+        W += price.shift(i) * (i+1)
+
+    return - W / price.rolling(block*n).sum()
+
+
+
+#Double exponential moving average
+def DEMA(price, block=10, window=20):
+    L = price.rolling(block).min()
+    H = price.rolling(block).max()
+    M = (H+L)/2
+
+    DEMA = 2 * M.ewm(block*window).mean() - M.ewm(block*window).mean().ewm(block*window).mean()
+    return DEMA
+
+
+
+#Detrended price oscillator
+def DPO(price, block=10, window=20):
+    A = price.shift(block*window//2 + 1)
+    B = price.rolling(block*window).mean()
+
+    return A-B
+
+
+
+#Weighted Moving Average
+def WMA(price, block = 10, window = 10):
+    W = price.copy() * block*window
+    d = 1
+
+    for i in range(1, block*window):
+        W += price.shift(i) * (block*window - i)
+        d += (i+1)
+
+    return W/d
+
+
+
+#Hull Moving Average
+def HullMA(price, block=10, window=10):
+    n = block * window
+    WMA1 = WMA(price.shift(n//2), block=block, window = window)
+    WMA2 = WMA(price.shift(n), block=block, window = window)
+
+    HMA = WMA(price = 2*WMA1-WMA2, block = int(np.sqrt(block)), window = int(np.sqrt(window)))
+
+    return HMA
+
+
+
+#Internal bar strength
+def IBS(price, block=10):
+
+    L = price.rolling(block).min()
+    H = price.rolling(block).max()
+
+    return (price - L)/(H - L)
+
+
+
+#Percentage price oscillator
+def PPO(price, block=10, small_window=12, big_window=26):
+
+    MACD = price.ewm(small_window*block).mean() - price.ewm(big_window*block).mean()
+    PPO = MACD/price.ewm(big_window*block).mean()*100
+
+    return PPO
